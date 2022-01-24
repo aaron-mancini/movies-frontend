b4fb86ec84cf3881643f32c1819e1c107e12ac16
aaron-mancini <aaronmancini2301@gmail.com>
2022-01-24 11:59:44 -0500

    test adjustments

--- client/src/movies/MovieDetails.test.js
+++ client/src/movies/MovieDetails.test.js
@@ -2,11 +2,14 @@ import React from "react";
 import { render } from "@testing-library/react";
 import MovieDetails from "./MovieDetails";
 import { MemoryRouter } from "react-router";
+import { UserProvider } from "../testUtils";
 
 it("renders without crashing", function () {
     render(
         <MemoryRouter>
+          <UserProvider>
             <MovieDetails />
+          </UserProvider>
         </MemoryRouter>,
     );
 });
