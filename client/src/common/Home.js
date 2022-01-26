86510ab0abc771646d59043bd9166ceedf7dc3c4
aaron-mancini <aaronmancini2301@gmail.com>
2022-01-26 17:34:49 -0500

    Update Home.js

--- client/src/common/Home.js
+++ client/src/common/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "reactstrap";
 
 const Home = () => {
     return (
         <div>
-            
+            <h1>Welcome to FilmRate</h1>
+            <h6>Sign up to create your own film reviews!</h6>
+            <h6>Or start searching for your favorite film now!</h6>
+            <Button href="/signup">Signup</Button>
         </div>
     )
 }
